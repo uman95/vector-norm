660370e25a09cf80ed5d29071bf4732ae4fcbf85
Usman Abdulganiy Babatunde <ausman@aimsammi.com>
2021-12-09 00:48:08 +0100

    still-setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "email = \"usman.aganiy@gmail.com\""  >> ~/.streamlit/credentials.toml
 echo "[server]"  > ~/.streamlit/config.toml 
 echo "headless = true"  >> ~/.streamlit/config.toml
 echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = true"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
 
 
 # mkdir -p ~/.streamlit/echo "\
